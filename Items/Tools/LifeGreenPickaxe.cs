64278613f93af12acd920fe753cefbf5fcd89463
Max Ku <wingfku2@gmail.com>
2018-08-27 13:27:13 +0800

    Update

---
+++ Items/Tools/LifeGreenPickaxe.cs
@@ -0,0 +1,53 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VinesMod.Items.Tools
+{
+	public class LifeGreenPickaxe : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("LifeGreen Pickaxe");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 25;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.pick = 200;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 5;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.LifeFruit, 5);
+			recipe.AddIngredient(ItemID.Emerald, 15);
+			recipe.AddRecipeGroup("IronBar", 12);
+			recipe.AddRecipeGroup("Wood", 40);
+			recipe.AddIngredient(mod, "ShardGreen", 35);
+			recipe.AddTile(mod.TileType("StarForge"));
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			if (Main.rand.Next(10) == 0)
+			{
+				int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, mod.DustType("SparkleBlue"));
+			}
+		}
+	}
+}
