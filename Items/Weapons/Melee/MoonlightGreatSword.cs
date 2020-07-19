1da8f477dfcfdb09f7e362f29f251f64e2024682
Vines <34955859+vinesmsuic@users.noreply.github.com>
2020-07-20 04:25:11 +0800

    update

---
+++ Items/Weapons/Melee/MoonlightGreatSword.cs
@@ -0,0 +1,60 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VinesMod.Items.Weapons.Melee
+{
+	public class MoonlightGreatSword : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Moonlight GreatSword");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 27;      
+			item.melee = true; 
+			item.width = 40; 
+			item.height = 40;           
+			item.useTime = 20;         
+			item.useAnimation = 20; 
+			item.useStyle = 1;//The use style of weapon, 1 for swinging, 2 for drinking, 3 act like shortsword, 4 for use like life crystal, 5 for use staffs or guns
+			item.knockBack = 3;
+			item.value = Item.sellPrice(copper: 60);           //The value of the weapon
+			item.rare = 1;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddRecipeGroup("IronBar", 15);
+			recipe.AddIngredient(ItemID.GoldBar, 12);
+			recipe.AddIngredient(mod, "ShardBlue", 5);
+			recipe.AddIngredient(mod, "ShardYellow", 5);
+			recipe.AddIngredient(mod, "ShardWhite", 5);
+			recipe.AddTile(mod.TileType("StarForge"));
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+		public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+			if (Main.rand.Next(5) == 0)
+			{
+				target.AddBuff(BuffID.Chilled, 60 * 5);
+			}
+        }
+
+		public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			if (Main.rand.Next(15) == 0)
+			{
+				Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, mod.DustType("SparkleBlue"));
+			}
+		}
+	}
+}
